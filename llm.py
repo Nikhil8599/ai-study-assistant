a94aeb35a1894740da3791c4061088c944105000
Nikhil Kumar Jha <nkjha197410@gmail.com>
2026-04-18 12:14:04 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv
 import os
 from langchain_groq import ChatGroq
-
+import streamlit as st
 load_dotenv()
 
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
-    groq_api_key=os.getenv("GROQ_API_KEY")
+    groq_api_key=st.secrets("GROQ_API_KEY")
 )
